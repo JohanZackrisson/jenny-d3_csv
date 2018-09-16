88dcce525846e3f053d4b3744230c099e90712f4
Johan Zackrisson <johanz@gmail.com>
2018-09-16 21:05:45 +0200

    parse

--- public/csv.js
+++ public/csv.js
@@ -1,25 +1,22 @@
-var read_csv = function()
-{
-    //var parse = d3.time.format("%m/%d/%Y").parse;
-    d3.csv("TEMP.csv", function(temperatures)
-    {
-        //prices is an array of json objects containing the data in from the csv
-        console.log("temperatures:", temperatures)
-        data = prices.map(function(d)
-        {
-            //each d is one line of the csv file represented as a json object
-            console.log("d", d)
-            month = parse(d.month).getMonth();
-            console.log("month:", d.month, month)
-            //we slice the dollar sign off then convert to a number with the + sign
-            //slicing works like "$216".slice(1) gives you 216, 
-            //you can also give it a range like "$216 asdf".slice(1,4) gives you 216
-            p = d.price
-            price = +p.slice(1)
-            console.log("price:", p, price);
-            return {"month": month, "value":price} ;
-        })
-        console.log("data", data)
-        bars(data);
+var read_csv = function () {
+
+    d3.csv("TEMP.csv").then( (data) => {
+        console.log(data)
+
+        // normally a csv contains the column row as the first row of the file, but in this case it things our start time is a column
+        const colName = data.columns[0]
+        const start = +colName
+        // the next row in the data in the sampling frequency
+        const freq = +data[0][colName]
+
+        // after that, all the samples follow
+        const samples = data.slice(1)
+
+        return samples.map( (sample, idx) => ({
+            time: start + (1.0/freq) * idx,
+            value: +sample[colName]
+        }))
+    }).then( (samples) => {
+        console.log(samples)
     })
 }
